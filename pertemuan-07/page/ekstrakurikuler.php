1804fb32234418425ffdc68b84e5600e4a17ad58
2522500059-source <2522500059.mahasiswa.atmaluhur.ac.id>
2026-04-30 21:24:15 +0700

    menamnahkan file ekstrakulier.php di page, membuat tabel data query dan lain lain

---
+++ pertemuan-07/page/ekstrakurikuler.php
@@ -0,0 +1,69 @@
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0 text-dark">Data Ekstrakurikuler</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+if (isset($_GET['action'])) {
+    if ($_GET['action'] == "hapus") {
+        $kd = $_GET['kd'];
+        $query = mysqli_query($koneksi, "DELETE FROM ekstrakurikuler WHERE kd_ekskul='$kd'");
+        if ($query) {
+            echo '<div class="alert alert-warning alert-dismissible">
+    Berhasil dihapus!</div>';
+            echo '<meta http-equiv="refresh" content="1;url=index.php?page=ekstrakurikuler">';
+        }
+    }
+}
+?>
+<div class="content">
+    <div class="container-fluid">
+        <div class="card">
+            <div class="card-body">
+                <a href="index.php?page=tambah_ekstrakurikuler" class="btn btn-primary btn-sm">Tambah Ekstrakurikuler</a>
+                <table class="table table-striped">
+                    <tread>
+                        <tr>
+                            <th>No</th>
+                            <th>Kd Ekstrakurikuler</th>
+                            <th>Nama Ekstrakurikuler</th>
+                            <th>Pembimbing 1</th>
+                            <th>Pembimbing 2</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </tread>
+                    <?php
+                    $no = 0;
+                    $query = mysqli_query($koneksi, "SELECT * FROM ekstrakurikuler");
+                    while ($result = mysqli_fetch_array($query)) {
+                        $no++;
+                        ?>
+                        <tbody>
+                            <tr>
+                                <td><?= $no; ?></td>
+                                <td><?= $result['kd_ekskul']; ?></td>
+                                <td><?= $result['nm_ekskul']; ?></td>
+                                <td><?= $result['pembimbing_1']; ?></td>
+                                <td><?= $result['pembimbing_2']; ?></td>
+                                <td>
+                                    <a href="index.php?page=ekstrakurikuler&action=hapus&kd=<?= $result['kd_ekskul']
+                                                                                    ?>" title="">
+                                        <span class="badge badge-danger">Hapus</span>
+                                    </a>
+                                    <a href="index.php?page=edit_ekstrakurikuler&kd=<?= $result['kd_ekskul'] ?>" title="">
+                                        <span class="badge badge-success">Edit</span>
+                                    </a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    <?php } ?>
+                </table>
+            </div>
+        </div>
+    </div>
+    
